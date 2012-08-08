f67162ed3497c24b0cfc37eb4e80a5cbb630b149
Martin Lindén <bmelinden@dbb.su.se>
2012-08-08 23:46:54 +0200

    Removed some spaces in directory names and updated vpSPTstart.m

--- vbSPTstart.m
+++ vbSPTstart.m
@@ -1,11 +1,12 @@
 % Add paths that are needed to run the VB3 analysis 
 %
 % F.P. 2012-07-04
+% M.L. 2012-08-08 : slight changes to reflect new folder names
 
 dir0=pwd;
 addpath(genpath([dir0 filesep '.' filesep 'HMMcode' filesep 'VB3']))
 addpath(genpath([dir0 filesep '.' filesep 'HMMcode' filesep 'HMMcore']))
 addpath(genpath([dir0 filesep '.' filesep 'HMMcode' filesep 'Tools']))
 addpath([dir0 filesep '.'])
-disp('Added local VB3 paths')
+disp('Added local vbSPT paths')
 disp('---------------------')
