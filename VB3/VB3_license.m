e1e29ffcc01392bd66f0c5398da1f96432131b93
Fredrik Persson <freddie.persson@gmail.com>
2012-08-10 11:19:30 +0200

    Added a command line short license text function

---
+++ VB3/VB3_license.m
@@ -0,0 +1,34 @@
+function VB3_license(funcName)
+% res=VB3_license(funcName)
+%?
+% Prints a short license text to the command line.
+
+%% copyright notice
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% VB3_license.m, prints a short license text for the vbSPT package
+% =========================================================================
+% 
+% Copyright (C) 2012 Martin Lind??n and Fredrik Persson
+% 
+% E-mail: bmelinden@gmail.com, freddie.persson@gmail.com
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation, either version 3 of the License, or any later
+% version.   
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+% Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program. If not, see <http://www.gnu.org/licenses/>.
+%% start of actual code
+
+fprintf(['vbSPT, ' funcName ', Copyright (C) 2012 Martin Linden and Fredrik Persson \n',...
+'This program comes with ABSOLUTELY NO WARRANTY; for details see license.txt. \n']);
+
+end
+
+
+
