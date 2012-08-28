c0e9be8f8c60e5503029a688e946b05b4b27505c
Fredrik Persson <freddie.persson@gmail.com>
2012-08-28 10:18:39 +0200

    Fixed the vbSPTstart file so the right folders are added to the matlab path.

--- vbSPTstart.m
+++ vbSPTstart.m
@@ -2,9 +2,9 @@
 % F.P. and M.L. 2012-08-08 
 
 dir0=pwd;
-addpath(genpath([dir0 filesep '.' filesep 'HMMcode' filesep 'VB3']))
-addpath(genpath([dir0 filesep '.' filesep 'HMMcode' filesep 'HMMcore']))
-addpath(genpath([dir0 filesep '.' filesep 'HMMcode' filesep 'Tools']))
+addpath(genpath([dir0 filesep '.' filesep 'VB3']))
+addpath(genpath([dir0 filesep '.' filesep 'HMMcore']))
+addpath(genpath([dir0 filesep '.' filesep 'Tools']))
 addpath([dir0 filesep '.'])
 disp('Added local vbSPT paths')
 disp('---------------------')
