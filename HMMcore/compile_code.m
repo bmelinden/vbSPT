2496de8c196f83a72cf9a602d32f9a6567219152
Martin Lindén <bmelinden@gmail.com>
2013-06-19 13:54:18 +0200

    removed plea to post new binaries to vbspt home page...

--- HMMcore/compile_code.m
+++ HMMcore/compile_code.m
@@ -9,7 +9,3 @@ for k=1:length(ff)
     disp(ff(k).name)
     mex(ff(k).name,['-' computer('arch')],'-O')    
 end
-
-Disp('If you compile binaries on systems that were not covered in the ' ...
-     'original distributions, please let us know by posting on the project ' ...
-     'home page, http://sourceforge.net/projects/vbspt/')
