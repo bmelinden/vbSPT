a84717d834287ba9bbeedb23ef44a9f0ed21f09b
Martin Lindén <bmelinden@gmail.com>
2017-01-19 16:32:11 +0100

    bugfix in vbSPTstart

--- vbSPTstart.m
+++ vbSPTstart.m
@@ -1,13 +1,11 @@
+function vbSPTstart()
 % Add paths that are needed to run the VB3 analysis 
-
-
 dir0=fileparts(mfilename('fullpath'));% path to this file, even if called from other folder
-addpath(genpath([dir0 filesep '.' filesep 'VB3']))
-addpath(genpath([dir0 filesep '.' filesep 'HMMcore']))
-addpath(genpath([dir0 filesep '.' filesep 'Tools']))
-addpath(genpath([dir0 filesep '.' filesep 'external']))
-addpath([dir0 filesep '.'])
-disp('Added local vbSPT paths')
-disp('---------------------')
-
-clear dir0
+addpath(dir0)
+addpath(fullfile(dir0,'VB3'))
+addpath(fullfile(dir0,'HMMcore'))
+addpath(fullfile(dir0,'Tools'))
+addpath(fullfile(dir0,'external'))
+disp('Added local vbSPT paths from')
+disp(dir0)
+disp('-----------------------')
